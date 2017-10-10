e3c7db15ac49558b09a2a25a9a3cbcc027b1eaf7
Miles Granger <miles59923@gmail.com>
2017-10-10 18:42:19 +0200

    final wrap up

--- core/ecs_service/start_scheduler.sh
+++ core/ecs_service/start_scheduler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-apt update && apt upgrade -y
+apt update -y
 apt install docker.io screen -y
 screen -dmS dask-scheduler-screen docker run --network=host milesg/tda-daskworker:latest dask-scheduler
