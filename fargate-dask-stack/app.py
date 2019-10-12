378cce50a5055dc9c55a5c7a69f0afeac202ca64
Miles Granger <miles59923@gmail.com>
2019-10-12 07:29:36 +0200

    Add jupyter service

--- fargate-dask-stack/app.py
+++ fargate-dask-stack/app.py
@@ -4,6 +4,7 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_ecs as ecs,
     aws_servicediscovery as servicediscovery,
+    aws_elasticloadbalancingv2 as elb,
     aws_ecs_patterns as ecs_patterns,
     aws_route53 as route53,
     aws_logs as logs,
@@ -16,8 +17,6 @@ class DaskStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         self.setup_networking()
-        self.setup_security()
-
         self.cluster = ecs.Cluster(
             self,
             id="DaskCluster",
@@ -29,8 +28,11 @@ class DaskStack(core.Stack):
                 vpc=self.vpc,
             ),
         )
+        self.setup_load_balancer()
         self.create_worker_service()
         self.create_scheduler_service()
+        self.create_jupyter_service()
+        self.setup_allowable_connections()
 
     def setup_networking(self):
         self.vpc = ec2.Vpc(
@@ -43,53 +45,22 @@ class DaskStack(core.Stack):
             enable_dns_hostnames=True,
         )
 
-    def setup_security(self):
-        # Security group
-        self.securitygroup = ec2.CfnSecurityGroup(
-            self,
-            id="sg-daskSecurityGrp",
-            group_description="Security Group for Dask",
-            vpc_id=self.vpc.vpc_id,
+    def setup_allowable_connections(self):
+        self.scheduler_service.connections.allow_from(
+            self.worker_service, ec2.Port.all_tcp()
+        )
+        self.scheduler_service.connections.allow_from(
+            self.jupyter_service, ec2.Port.tcp(port=8786)
+        )
+        self.worker_service.connections.allow_from(
+            self.scheduler_service, ec2.Port.all_tcp()
         )
 
-        # Ingress to allow all resources associated with this security group to talk
-        security_group_free_ingress = ec2.CfnSecurityGroupIngress(
-            self,
-            id="daskSecurityIngress",
-            ip_protocol="tcp",
-            description="Allow all resources w/ this security group to talk",
-            from_port=0,
-            to_port=65535,
-            group_id=self.securitygroup.ref,
-            source_security_group_id=self.securitygroup.attr_group_id,
+    def setup_load_balancer(self):
+        self.elb = elb.ApplicationLoadBalancer(
+            self, id="jupyterLoadBalancer", internet_facing=True, vpc=self.vpc
         )
-        security_group_free_ingress.add_depends_on(self.securitygroup)
-
-        # Ingress to allow web views for Jupyter and Bokeh dashboard
-        security_group_web_ingress = ec2.CfnSecurityGroupIngress(
-            self,
-            id="daskSecurityIngressWeb",
-            ip_protocol="tcp",
-            description="Allow ingress to Jupyter and Bokeh",
-            from_port=8786,
-            to_port=8888,
-            group_id=self.securitygroup.ref,
-            source_security_group_id=self.securitygroup.attr_group_id,
-        )
-        security_group_web_ingress.add_depends_on(self.securitygroup)
-
-        # All egress
-        security_group_free_egress = ec2.CfnSecurityGroupEgress(
-            self,
-            id="daskSecurityEgress",
-            ip_protocol="tcp",
-            description="Allow all egress",
-            from_port=0,
-            to_port=65535,
-            group_id=self.securitygroup.ref,
-            destination_security_group_id=self.securitygroup.attr_group_id,
-        )
-        security_group_free_egress.add_depends_on(self.securitygroup)
+        self.listener = self.elb.add_listener("PublicListener", port=80, open=True)
 
     def create_worker_service(self):
         worker_task_definition = ecs.FargateTaskDefinition(
@@ -128,16 +99,15 @@ class DaskStack(core.Stack):
                 container_port=5555, host_port=5555, protocol=ecs.Protocol.TCP
             ),
         )
-        worker_service = ecs.FargateService(
+        self.worker_service = ecs.FargateService(
             self,
             id="worker",
             cluster=self.cluster,
             desired_count=2,
             service_name="worker",
             task_definition=worker_task_definition,
-            security_group=self.securitygroup,
         )
-        worker_service.enable_cloud_map(
+        self.worker_service.enable_cloud_map(
             dns_record_type=servicediscovery.DnsRecordType.A, name="worker"
         )
 
@@ -170,18 +140,81 @@ class DaskStack(core.Stack):
             ),
         )
 
-        scheduler_service = ecs.FargateService(
+        self.scheduler_service = ecs.FargateService(
             self,
             id="scheduler",
             cluster=self.cluster,
             desired_count=1,
             service_name="scheduler",
             task_definition=scheduler_task_definition,
-            security_group=self.securitygroup,
         )
-        scheduler_service.enable_cloud_map(
+        self.scheduler_service.enable_cloud_map(
             dns_record_type=servicediscovery.DnsRecordType.A, name="scheduler"
         )
+
+    def create_jupyter_service(self):
+        jupyter_task_definition = ecs.FargateTaskDefinition(
+            self, id="jupyterTaskDefinition", cpu=1024, memory_limit_mib=2048
+        )
+        container = jupyter_task_definition.add_container(
+            id="jupyterContainer",
+            cpu=1024,
+            memory_limit_mib=2048,
+            essential=True,
+            image=ecs.ContainerImage.from_registry(
+                "docker.io/milesg/tda-daskworker:latest"
+            ),
+            command=[
+                "jupyter",
+                "notebook",
+                "--NotebookApp.token=supersecret",
+                "--ip",
+                "0.0.0.0",
+                "--no-browser",
+                "--allow-root",
+            ],
+            logging=ecs.LogDriver.aws_logs(
+                stream_prefix="jupyter-", log_retention=logs.RetentionDays.ONE_DAY
+            ),
+        )
+        container.add_port_mappings(
+            ecs.PortMapping(
+                container_port=8888, host_port=8888, protocol=ecs.Protocol.TCP
+            )
+        )
+
+        self.jupyter_service = ecs.FargateService(
+            self,
+            id="jupyter",
+            cluster=self.cluster,
+            desired_count=1,
+            service_name="jupyter",
+            task_definition=jupyter_task_definition,
+            health_check_grace_period=core.Duration.seconds(120),
+            assign_public_ip=True,
+        )
+        self.jupyter_service.enable_cloud_map(
+            dns_record_type=servicediscovery.DnsRecordType.A, name="jupyter"
+        )
+
+        healthcheck = elb.HealthCheck(
+            interval=core.Duration.seconds(60),
+            path="/",
+            timeout=core.Duration.seconds(40),
+            port="8888",
+            healthy_http_codes="200-399",
+        )
+
+        atg = elb.ApplicationTargetGroup(
+            self,
+            id="appTargetGroup",
+            port=8888,
+            vpc=self.vpc,
+            protocol=elb.ApplicationProtocol.HTTP,
+            targets=[self.jupyter_service],
+            health_check=healthcheck,
+        )
+        self.listener.add_target_groups(id="jupyterTargetgroups", target_groups=[atg])
 
 
 app = core.App()
